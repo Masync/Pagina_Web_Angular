540733be65641871392966d0523bdb4605b871ce
Juan Manuel Patiño <jumapa662@gmail.com>
2019-11-24 19:49:28 -0500

    Pagina Web
    Creacion de pagina web para empresa de comida.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { CatalogosComponent } from './componentes/catalogos/catalogos.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'catalogo', component: CatalogosComponent},
+  {path: 'contactos', component: ContactosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { CatalogosComponent } from './componentes/catalogos/catalogos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServiciosComponent,
+    ContactosComponent,
+    CatalogosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
